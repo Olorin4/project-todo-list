1dd64956695cbf8f418629fa7b61e66a95c51bad
Nick Kalas <nick.kalas@proton.me>
2024-07-05 15:36:24 +0300

    Adjust styles.css

--- src/index.js
+++ src/index.js
@@ -5,3 +5,13 @@ import { loadMainContent } from './MainContent';
 document.addEventListener('DOMContentLoaded', () => {
     loadMainContent();
 });
+
+
+
+
+
+
+// TO DO:
+// - Make inputs editable on double click.
+// - Add delete project button, visible only when input is hovered
+// - Add task button with input field.
